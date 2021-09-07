734ce34f06d26d4b947a75baa02495c5499ca51a
lalaavipsha <53574326+lalaavipsha@users.noreply.github.com>
2021-09-07 20:47:08 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+    jQuery('.burger-menu').on('click',function(){
+        jQuery('.mob-nav').toggle();
+    })
+});/*query document load howar por bakita load hobe*/
